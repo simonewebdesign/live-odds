1f3076f2dd1a3d1474c7be0a87713390b341b624
Simone Vittori <hello@simonewebdesign.it>
2017-02-02 22:34:08 +0000

    Starting the journey

---
+++ lib/live_odds.ex
@@ -0,0 +1,18 @@
+defmodule LiveOdds do
+  @moduledoc """
+  Documentation for LiveOdds.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LiveOdds.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/live_odds_test.exs
@@ -0,0 +1,8 @@
+defmodule LiveOddsTest do
+  use ExUnit.Case
+  doctest LiveOdds
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
