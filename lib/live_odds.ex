0cbfb5b262e412a9223b25f33d0efb5603dd3038
Simone Vittori <hello@simonewebdesign.it>
2017-02-11 23:32:47 +0000

    Remove sample code

--- lib/live_odds.ex
+++ lib/live_odds.ex
@@ -1,18 +1,2 @@
 defmodule LiveOdds do
-  @moduledoc """
-  Documentation for LiveOdds.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> LiveOdds.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
