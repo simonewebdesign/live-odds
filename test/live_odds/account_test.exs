2094a3d2a9008b9a00c1963b601aebaf1bd12d59
Simone Vittori <hello@simonewebdesign.it>
2017-02-18 19:10:45 +0000

    Account balance using patterns
    Hiding the fact that we are dealing with a process, and accessing its services through a functional interface.

--- test/live_odds/account_test.exs
+++ test/live_odds/account_test.exs
@@ -4,71 +4,45 @@ defmodule AccountTest do
   alias LiveOdds.Account
 
 
-  # test "it works" do
-  #   :ok = Account.start()
-  #   assert 0 == Account.balance()
-  #   :ok = Account.credit(50)
-  #   assert 50 == Account.balance()
-  #   :ok = Account.credit(100)
-  #   :ok = Account.debit(30)
-  #   assert 120 == Account.balance()
-  #   :ok = Account.stop()
-  # end
-
   setup do
     :ok = Account.start()
-  end
 
-
-  test "close an account" do
-    :ok = Account.stop()
+    on_exit(fn() ->
+      :ok = Account.stop()
+    end)
   end
 
 
   test "ask for current balance" do
-
+    assert 0 == Account.balance()
   end
 
 
-  test "credit" do
-
+  test "deposit" do
+    assert :ok == Account.credit(50)
+    assert 50 == Account.balance()
   end
 
 
-  test "debit" do
-
+  test "deposit and withdraw" do
+    assert :ok == Account.credit(100)
+    assert :ok == Account.debit(30)
+    assert 70 == Account.balance()
   end
 
-  # test "deposit and then withdraw" do
-  #   {:ok, pid} = Account.start()
-  #   msg1 = {:credit, 50}
-  #   msg2 = {:debit, 30}
-  #   send(pid, {msg1, self()})
-  #   send(pid, {msg2, self()})
 
-  #   assert_receive(:ok)
-  #   assert_receive(:ok)
-  # end
+  test "depositing a negative amount should error" do
+    {:error, msg} = Account.credit(-1)
+
+    assert msg == "not a positive credit"
+  end
 
 
-  # test "depositing a negative amount should error" do
-  #   {:ok, pid} = Account.start()
-  #   msg = {:credit, -1}
-  #   send(pid, {msg, self()})
+  test "withdrawing more than the availability should error" do
+    :ok = Account.credit(50)
+    {:error, msg} = Account.debit(60)
 
-  #   assert_receive({:error, "not a positive credit"})
-  # end
-
-
-  # test "withdrawing more than the availability should error" do
-  #   {:ok, pid} = Account.start()
-  #   msg1 = {:credit, 50}
-  #   msg2 = {:debit, 60}
-  #   send(pid, {msg1, self()})
-  #   send(pid, {msg2, self()})
-
-  #   assert_receive(:ok)
-  #   assert_receive({:error, "not enough money"})
-  # end
+    assert msg == "not enough money"
+  end
 
 end
