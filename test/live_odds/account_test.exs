f109ff0cb210d8bcc318284fe9ffaa48a000e134
Simone Vittori <hello@simonewebdesign.it>
2017-02-18 19:16:51 +0000

    Account balance using patterns (#1)
    Hiding the fact that we are dealing with a process, and accessing its services through a functional interface.

--- test/live_odds/account_test.exs
+++ test/live_odds/account_test.exs
@@ -4,54 +4,45 @@ defmodule AccountTest do
   alias LiveOdds.Account
 
 
+  setup do
+    :ok = Account.start()
+
+    on_exit(fn() ->
+      :ok = Account.stop()
+    end)
+  end
+
+
   test "ask for current balance" do
-    {:ok, pid} = Account.start()
-    msg = :balance
-    send(pid, {msg, self()})
-
-    assert_receive(0)
+    assert 0 == Account.balance()
   end
 
 
-  test "credit" do
-    {:ok, pid} = Account.start()
-    msg = {:credit, 50}
-    send(pid, {msg, self()})
-
-    assert_receive(:ok)
+  test "deposit" do
+    assert :ok == Account.credit(50)
+    assert 50 == Account.balance()
   end
 
 
-  test "deposit and then withdraw" do
-    {:ok, pid} = Account.start()
-    msg1 = {:credit, 50}
-    msg2 = {:debit, 30}
-    send(pid, {msg1, self()})
-    send(pid, {msg2, self()})
-
-    assert_receive(:ok)
-    assert_receive(:ok)
+  test "deposit and withdraw" do
+    assert :ok == Account.credit(100)
+    assert :ok == Account.debit(30)
+    assert 70 == Account.balance()
   end
 
 
   test "depositing a negative amount should error" do
-    {:ok, pid} = Account.start()
-    msg = {:credit, -1}
-    send(pid, {msg, self()})
+    {:error, msg} = Account.credit(-1)
 
-    assert_receive({:error, "not a positive credit"})
+    assert msg == "not a positive credit"
   end
 
 
   test "withdrawing more than the availability should error" do
-    {:ok, pid} = Account.start()
-    msg1 = {:credit, 50}
-    msg2 = {:debit, 60}
-    send(pid, {msg1, self()})
-    send(pid, {msg2, self()})
+    :ok = Account.credit(50)
+    {:error, msg} = Account.debit(60)
 
-    assert_receive(:ok)
-    assert_receive({:error, "not enough money"})
+    assert msg == "not enough money"
   end
 
 end
